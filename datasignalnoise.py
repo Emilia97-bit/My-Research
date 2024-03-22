fb8bc1a48ae22aae1346496088e4268a8d4da45a
Emilia97-bit <54474492+Emilia97-bit@users.noreply.github.com>
2024-03-22 15:14:36 +0200

    Add files via upload

---
+++ datasignalnoise.py
@@ -0,0 +1,55 @@
+import matplotlib.pylab as pylab
+import matplotlib as mpl
+mpl.use('Svg')
+import numpy as np
+import matplotlib.pyplot as plt
+import ruptures as rpt
+plt.style.use('tableau-colorblind10')
+
+
+params = {'legend.fontsize': 'xx-large',
+         'axes.labelsize': 'xx-large',
+          'axes.titlesize':'xx-large',
+          'xtick.labelsize':'xx-large',
+          'ytick.labelsize':'xx-large'}
+
+pylab.rcParams.update(params)
+
+from ctapipe.io import EventSource
+from ctapipe.utils import get_dataset_path
+from ctapipe.visualization import CameraDisplay
+
+particle = 'Gamma'
+if particle=="Gamma":
+    source = EventSource("/lfs/l7/hess/users/steinmassl/MC_phase2d3_production/Diffuse_benedetta_new/ref0.79/"+particle+"/20deg/0deg/0.0deg/Data/gamma_20deg_0deg_run21410___phase2d_simon3_desert-all_tel-_mix_atm_new_trig_thresh-sims-realnsb_benedettanew0903_cone5.simhess.gz",focal_length_choice='EQUIVALENT')
+elif particle=="Proton":
+    source = EventSource("/lfs/l7/hess/users/steinmassl/MC_phase2d3_production/Diffuse_benedetta_new/ref0.79/"+particle+"/20deg/0deg/0.0deg/Data/proton_20deg_0deg_run21610___phase2d_simon3_desert-all_tel-_mix_atm_new_trig_thresh-sims-realnsb_benedettanew0903.simhess.gz",focal_length_choice='EQUIVALENT')
+
+
+                                                                 
+event_iterator = iter(source)
+
+for i in np.arange(5):
+    event = next(event_iterator)
+    
+    teldata = event.r0.tel[5]
+
+
+    # generate signal                                                           
+    n_samples, dim, sigma = 1000, 3, 4
+    n_bkps = 4
+
+    if teldata.waveform is not None:
+        algo = rpt.Pelt(model="l2").fit(teldata.waveform[0,0,:])
+        result = algo.predict(pen=2)
+        print(result)
+
+    # display                                                                   
+        rpt.display(teldata.waveform[0,0,:], result)
+
+    plt.xlabel("Sample")
+    plt.ylabel("Amplitude\n [p.e.]")
+
+
+    plt.tight_layout()
+    plt.savefig('Waveformrl2pen2'+str(i)+'.png')
