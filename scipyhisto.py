5d9a7953c08ac7958877e782123a311019121833
Emilia97-bit <54474492+Emilia97-bit@users.noreply.github.com>
2024-05-07 16:15:51 +0200

    Add files via upload

--- scipyhisto.py
+++ scipyhisto.py
@@ -7,14 +7,20 @@ import matplotlib.pyplot as plt
 gamma_data = np.load('gamma.npy')
 proton_data = np.load('proton.npy')
 
+#print(np.load('gamma.npy'))
+#print(np.load('proton.npy'))
+
 # Plot histogram                                                                                                                                              
 plt.figure(figsize=(20, 10))
 plt.hist([gamma_data, proton_data], bins=range(0, max(max(gamma_data), max(proton_data)) + 1), alpha=0.7, label=['Gamma', 'Proton'])
-#plt.hist([gamma_data, proton_data], bins=range(1, 26), alpha=0.7, label=['Gamma', 'Proton'])
-#plt.xlim(0,25)
-#plt.ylim(0,2)
-plt.xlabel('Peaks')
-plt.ylabel('Number of changes per event')
+#plt.hist([gamma_data, proton_data], bins=20, alpha=0.7, label=['Gamma', 'Proton'])
+
+#plt.hist(gamma_data, bins=20, alpha=0.7, label='Gamma')
+#plt.hist(proton_data, bins=20, alpha=0.7, label='Proton')
+plt.xlim(5400,6800)
+plt.ylim(0,16)
+plt.xlabel('Number of peaks per event')
+plt.ylabel('Number of event')
 plt.title('Histogram of Change Points')
 plt.legend()
-plt.savefig('Histogram of peak finding')
+plt.savefig('Histogram of peaks finding2')
